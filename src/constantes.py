c9968510e12c70ec0ff5c0290a282148296f203f
Onácio Santana <nsonacio@hotmail.com>
2024-04-14 16:13:47 -0300

    limpeza

--- src/constantes.py
+++
@@ -1,8 +0,0 @@
-CRAR_TABELA_PEDIDOS = '''
-    CREATE TABLE IF NOT EXISTS pedidos (
-        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-        descricao TEXT NOT NULL,
-        unidade TEXT NOT NULL,
-        data TEXT NOT NULL
-    );
-'''
--- src/views/home.py
+++ src/views/home.py
@@ -1,15 +1,11 @@
 from flask import Blueprint, session, render_template, redirect, url_for
-from models.pedidos_model import Pedido
+
 
 home = Blueprint('home', __name__)
 
 @home.route('/')
-def index():
-    print('rota inicial (/)')
-    if 'username' in session:
-        pedido = Pedido("Pedido de teste maior que o anterios", "Nagé", "01/01/2023")
-        pedido.inserir()
+def index():    
+    if 'username' in session:        
         return render_template('home/index.html')
-    
     
     return redirect(url_for('auth.login'))
