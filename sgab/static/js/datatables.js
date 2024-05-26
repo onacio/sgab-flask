357ab0ab0c7cc06947862e67130759a5bc341a24
Onácio Santana <nsonacio@hotmail.com>
2024-05-26 15:36:48 -0300

    melhorias no visual

---
+++ sgab/static/js/datatables.js
@@ -0,0 +1,11 @@
+$(document).ready( function () {
+    $('#tabela').DataTable({
+      searching: true,
+      ordering: true,
+      paging: true,
+      language: {
+        url:"https://cdn.datatables.net/plug-ins/1.11.3/i18n/pt_br.json"
+      }
+      //scrollY: 380
+    });
+} );
